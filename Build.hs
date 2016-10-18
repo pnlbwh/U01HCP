4603abf9da47fe86a61e2737314843c0117bcd91
reckbo <reckbo@bwh.harvard.edu>
2016-10-18 12:33:13 +1100

    split hcp dwi for yogesh to inspect

--- Build.hs
+++ Build.hs
@@ -2,7 +2,7 @@
 {-# LANGUAGE DeriveGeneric              #-}
 {-# LANGUAGE FlexibleInstances          #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-import qualified Nrrd        (fa, makeMask, mask)
+import FSL
 import           PNLPipeline
 
 outdir = "_data"
@@ -11,13 +11,25 @@ outdir = "_data"
 -- main
 main :: IO ()
 main = shakeArgs shakeOptions{shakeFiles=outdir, shakeVerbosity=Chatty} $ do
-  return ()
-    -- action $ do
-    --     let keys = [FaStats d m
-    --                | d <- [DwiHarm, DwiEd]
-    --                , m <- [FsMask, FsMaskNoCsf]
-    --                ]
-    --     apply keys :: Action [Double]
 
-    -- rule (buildKey :: (DwiType, CaseId) -> Maybe (Action Double))
-    -- rule (buildKey :: FaStats -> Maybe (Action Double))
+  want [outdir </> "data-1.nrrd",
+        outdir </> "data-2.nrrd"
+       ]
+
+  "_data/data-*.nrrd" %> \out -> do
+    need [tonii out]
+    dwiToNrrd ["--allowLossyConversion"] (tonii out)
+    return ()
+
+  [outdir </> "data-1.nii.gz",
+   outdir </> "data-2.nii.gz",
+   outdir </> "data-1.bval",
+   outdir </> "data-1.bvec",
+   outdir </> "data-2.bval",
+   outdir </> "data-2.bvec"]
+    &%> \(out1:out2:_) -> do
+    let dwi = "src/data.nii.gz"
+    need [dwi, tobval dwi, tobvec dwi]
+    (out1', out2') <- snipDwi dwi 100
+    moveDwi out1' out1
+    moveDwi out2' out2
